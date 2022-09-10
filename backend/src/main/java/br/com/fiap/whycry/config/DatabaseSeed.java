ff3aef4539292ac51bfbcbeb20f8316e2be547c9
Gabriel Jesus Dantas <gabrielrpg68@gmail.com>
2022-09-10 17:23:03 -0300

    Criada todas as classes

--- backend/src/main/java/br/com/fiap/whycry/config/DatabaseSeed.java
+++ backend/src/main/java/br/com/fiap/whycry/config/DatabaseSeed.java
@@ -10,12 +10,20 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.fiap.whycry.model.Agenda;
 import br.com.fiap.whycry.model.Arquivo;
+import br.com.fiap.whycry.model.Avaliacao;
 import br.com.fiap.whycry.model.Bebe;
+import br.com.fiap.whycry.model.Choro;
+import br.com.fiap.whycry.model.Classificacao;
 import br.com.fiap.whycry.model.Cliente;
+import br.com.fiap.whycry.model.Solucoes;
 import br.com.fiap.whycry.repository.AgendaRepository;
 import br.com.fiap.whycry.repository.ArquivoRepository;
+import br.com.fiap.whycry.repository.AvaliacaoRepository;
 import br.com.fiap.whycry.repository.BebeRepository;
+import br.com.fiap.whycry.repository.ChoroRepository;
+import br.com.fiap.whycry.repository.ClassificacaoRepository;
 import br.com.fiap.whycry.repository.ClienteRepository;
+import br.com.fiap.whycry.repository.SolucoesRepository;
 import br.com.fiap.whycry.service.ClienteService;
 
 @Configuration
@@ -28,7 +36,14 @@ public class DatabaseSeed implements CommandLineRunner {
     AgendaRepository repository3;
     @Autowired
     ArquivoRepository repository4;
-
+    @Autowired
+    AvaliacaoRepository repository5;
+    @Autowired
+    ClassificacaoRepository repository6;
+    @Autowired
+    ChoroRepository repository7;
+    @Autowired
+    SolucoesRepository repository8;
 
     ClienteService service;
 
@@ -51,8 +66,19 @@ public class DatabaseSeed implements CommandLineRunner {
         ));
 
         Arquivo arq1 = new Arquivo(bebe1, "Arquivo de detalhes", "Bebe precisa de atenção dobrada", LocalDate.of(2022, 06, 28));
-
         repository4.saveAll(List.of(arq1));
+
+        Avaliacao ava1 = new Avaliacao('S');
+        repository5.saveAll(List.of(ava1));
+
+        Classificacao clasificacao1 = new Classificacao("Exemplo de classificação");
+        repository6.saveAll(List.of(clasificacao1));
+
+        Choro choro1 = new Choro(bebe1, clasificacao1, ava1, "Fome");
+        repository7.saveAll(List.of(choro1));
+
+        Solucoes solucao1 = new Solucoes(clasificacao1, "Alimente", "De comida para a criança");
+        repository8.saveAll(List.of(solucao1));
     }
 
     
